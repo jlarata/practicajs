60c0537e6194256483430bd0d3e4df579852e1a3
Ariel Wasserman <ariel@cfee.lan>
2023-09-08 14:38:12 -0300

    first commit

---
+++ sintaxis.js
@@ -0,0 +1,14 @@
+let a;
+let x = "hola,soy x";
+const myVar = 3;
+var riberesdelab = true;
+var z = null;
+
+console.log("la variable a es del tipo "+typeof(a));
+console.log("la variable x es del tipo "+typeof(x));
+console.log("la variable myVar es del tipo "+typeof(myVar));
+console.log("la variable riberesdelab es del tipo "+typeof(riberesdelab));
+
+console.log("es riber de la b? respuesta: "+riberesdelab);
+
+console.log("la variable z es del tipo "+typeof(z)+"y da "+z);
